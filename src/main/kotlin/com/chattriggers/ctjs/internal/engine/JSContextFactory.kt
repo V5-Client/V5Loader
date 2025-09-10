baf1665f951b2247c0340161771071855c640928
Doc <65479309+DocilElm@users.noreply.github.com>
2025-09-10 15:26:35 -0700

    JSContextFactory: remove debugger

--- src/main/kotlin/com/chattriggers/ctjs/internal/engine/JSContextFactory.kt
+++ src/main/kotlin/com/chattriggers/ctjs/internal/engine/JSContextFactory.kt
@@ -3,11 +3,9 @@ package com.chattriggers.ctjs.internal.engine
 import com.chattriggers.ctjs.CTJS
 import com.chattriggers.ctjs.internal.launch.CTJavaObjectMappingProvider
 import org.mozilla.javascript.Context
-import org.mozilla.javascript.Context.EMIT_DEBUG_OUTPUT
 import org.mozilla.javascript.Context.FEATURE_LOCATION_INFORMATION_IN_ERROR
 import org.mozilla.javascript.ContextFactory
 import org.mozilla.javascript.WrapFactory
-import java.io.File
 import java.net.URL
 import java.net.URLClassLoader
 
@@ -20,7 +18,7 @@ object JSContextFactory : ContextFactory() {
     override fun onContextCreated(cx: Context) {
         super.onContextCreated(cx)
 
-        cx.debugOutputPath = File(".", "DEBUG")
+        // cx.debugOutputPath = File(".", "DEBUG")
         cx.applicationClassLoader = classLoader
         cx.optimizationLevel = if (optimize) 9 else 0
         cx.languageVersion = Context.VERSION_ES6
@@ -37,7 +35,7 @@ object JSContextFactory : ContextFactory() {
     override fun hasFeature(cx: Context?, featureIndex: Int): Boolean {
         when (featureIndex) {
             FEATURE_LOCATION_INFORMATION_IN_ERROR -> return true
-            EMIT_DEBUG_OUTPUT -> return CTJS.isDevelopment
+            // EMIT_DEBUG_OUTPUT -> return CTJS.isDevelopment
         }
 
         return super.hasFeature(cx, featureIndex)
