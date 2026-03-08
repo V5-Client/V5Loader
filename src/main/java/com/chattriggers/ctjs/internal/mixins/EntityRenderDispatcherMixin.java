080b5ce3fffda08020d5a1b7f99d4455c30f3384
qxionr <118845758+qxionr@users.noreply.github.com>
2026-03-08 11:46:39 +0000

    Revert "push entity mixins which will most likely crash the game!"
    This reverts commit 3cca5a5262c000a9196fe905c5cf1ef684f58518.

--- src/main/java/com/chattriggers/ctjs/internal/mixins/EntityRenderDispatcherMixin.java
+++ src/main/java/com/chattriggers/ctjs/internal/mixins/EntityRenderDispatcherMixin.java
@@ -6,7 +6,6 @@ import com.chattriggers.ctjs.api.render.Renderer;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.render.command.OrderedRenderCommandQueue;
 import net.minecraft.client.render.entity.EntityRenderManager;
-import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.render.state.CameraRenderState;
@@ -15,7 +14,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.resource.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -32,27 +30,8 @@ public abstract class EntityRenderDispatcherMixin {
         at = @At("HEAD"),
         cancellable = true
     )
-    private <S extends EntityRenderState> void injectRender(
-        S state,
-        CameraRenderState cameraState,
-        double x,
-        double y,
-        double z,
-        MatrixStack matrices,
-        OrderedRenderCommandQueue queue,
-        CallbackInfo ci
-    ) {
-        int id = ((EntityRenderStateAccessor) state).ctjs$getEntityId();
-        if (id != 0 && Client.getMinecraft().world != null) {
-            Entity entity = Client.getMinecraft().world.getEntityById(id);
-            if (entity != null) {
-                CTEvents.RENDER_ENTITY.invoker().render(
-                    matrices,
-                    entity,
-                    Client.getMinecraft().getRenderTickCounter().getDynamicDeltaTicks(),
-                    ci
-                );
-            }
-        }
+    private <S extends EntityRenderState> void injectRender(S renderState, CameraRenderState cameraRenderState, double d, double e, double f, MatrixStack matrixStack, OrderedRenderCommandQueue orderedRenderCommandQueue, CallbackInfo ci) {
+        // fixme: this technically works, however i'm unsure whether the targetedEntity is the right entity as code-wise it seems like it's the player/camera entity
+//        CTEvents.RENDER_ENTITY.invoker().render(matrixStack, targetedEntity, Client.getMinecraft().getRenderTickCounter().getDynamicDeltaTicks(), ci);
     }
 }
--- src/main/java/com/chattriggers/ctjs/internal/mixins/EntityRenderStateAccessor.java
+++
@@ -1,6 +0,0 @@
-package com.chattriggers.ctjs.internal.mixins;
-
-public interface EntityRenderStateAccessor {
-    void ctjs$setEntityId(int id);
-    int ctjs$getEntityId();
-}
