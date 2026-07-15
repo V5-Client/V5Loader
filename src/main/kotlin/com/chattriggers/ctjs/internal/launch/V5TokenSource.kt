8671770470aed7b1ed1e9e2eacf903c4f4be3d4a
rdbtCVS <87152661+rdbtCVS@users.noreply.github.com>
2026-07-15 23:17:59 +0100

    please fucking work

--- src/main/kotlin/com/chattriggers/ctjs/internal/launch/V5TokenSource.kt
+++ src/main/kotlin/com/chattriggers/ctjs/internal/launch/V5TokenSource.kt
@@ -1,6 +1,15 @@
 package com.chattriggers.ctjs.internal.launch
 
+import java.lang.reflect.Modifier
+
 internal object V5TokenSource {
+    private val initMethod by lazy {
+        runCatching {
+            Class.forName("com.v5.loader.internal.V5Loader").getMethod("init")
+                .takeIf { Modifier.isStatic(it.modifiers) }
+        }.getOrNull()
+    }
+
     private val consumeTokenMethod by lazy {
         runCatching {
             Class.forName("com.v5.loader.internal.V5Loader").getMethod("consumeToken")
@@ -8,6 +17,8 @@ internal object V5TokenSource {
     }
 
     @JvmStatic
-    fun consumeToken(): String? =
-        (consumeTokenMethod?.invoke(null) as? String)?.takeIf { it.isNotBlank() }
+    fun consumeToken(): String? {
+        initMethod?.invoke(null)
+        return (consumeTokenMethod?.invoke(null) as? String)?.takeIf { it.isNotBlank() }
+    }
 }
